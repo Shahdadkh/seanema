78a9c999a9590b8446a4cac23746824687ee80eb
ShahdadKh <mr.khoshbakht91@gmail.com>
2023-06-26 01:25:30 +0330

    Create StartPage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
+import StartPage from "@/components/Common/StartPage";
+
 export default function Home() {
-  return (
-    <div className='text-center font-bold text-2xl mt-5 underline text-yellow-500'>Hello World!</div>
-  )
+  return <StartPage />;
 }
