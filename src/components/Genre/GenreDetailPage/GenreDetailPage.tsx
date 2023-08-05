2adf567212a7b61f5987cf17b159328d5f68c89a
ShahdadKh <mr.khoshbakht91@gmail.com>
2023-08-05 16:56:00 +0330

    Complete GenreDetail Page

--- src/components/Genre/GenreDetailPage/GenreDetailPage.tsx
+++ src/components/Genre/GenreDetailPage/GenreDetailPage.tsx
@@ -10,14 +10,40 @@ import pic3 from "@/assets/images/mulan.png";
 import pic4 from "@/assets/images/iam-legend.png";
 
 const GenreDetailPage = () => {
+  const genreDetail = [
+    {
+      name: "Wonder Woman",
+      genre: "Comedy, Family",
+      image: pic1,
+      href: "",
+    },
+    {
+      name: "Alita",
+      genre: "Cyberpunk, Fantasy",
+      image: pic2,
+      href: "",
+    },
+    {
+      name: "Mulan",
+      genre: "Action, Drama",
+      image: pic3,
+      href: "",
+    },
+    {
+      name: "I am Legend",
+      genre: "Drama, Fantasy",
+      image: pic4,
+      href: "",
+    },
+  ];
   return (
-    <div className="relative w-full h-screen sm:w-3/12 mx-auto overflow-x-hidden backgroundColor1">
+    <div className="relative w-full h-full sm:w-3/12 mx-auto border backgroundColor1">
       <TitlePage title="Detail genre" />
       <div className="flex justify-center items-center relative top-14 sm:top-12 w-28 h-28 rounded-full mx-auto backgroundColor3">
         <Image src={action} alt="" width={40} height={40} />
       </div>
       <Image src={backImage} alt="" className="w-full h-fit" />
-      <div className="w-full h-fit backgroundColor3 overflow-hidden">
+      <div className="w-full h-full backgroundColor3 border borderColor2">
         <div className="text-white text-center text-3xl mt-5 font-medium">
           Action
         </div>
@@ -26,17 +52,24 @@ const GenreDetailPage = () => {
           are thrust into a series of events
         </div>
         <div className="mb-10">
-          <div className="grid grid-cols-2 grid-rows-1 gap-4 w-5/6 h-fit mt-4 mx-auto">
-            <div className="border row-span-4">1</div>
-            <div className="border row-span-1 h-48">2</div>
-            <div className="border row-span-4">3</div>
-            <div className="border row-span-1 h-48">4</div>
-          </div>
-          <div className="grid grid-cols-2 grid-rows-1 gap-4 w-5/6 h-fit mt-4 mx-auto">
-            <div className="border row-span-4">1</div>
-            <div className="border row-span-1 h-48">2</div>
-            <div className="border row-span-4">3</div>
-            <div className="border row-span-1 h-48">4</div>
+          <div className="grid grid-cols-2 gap-4 w-5/6 h-fit mt-4 mx-auto">
+            {genreDetail.map((genre, i) => (
+              <Link href={genre.href} key={i} className="h-96">
+                <div className="w-full h-[78%] mx-auto rounded-2xl overflow-hidden">
+                  <Image
+                    src={genre.image}
+                    alt={(i + 1).toString()}
+                    className="w-full h-full"
+                  />
+                </div>
+                <div className="text-white text-center text-2xl mt-2 font-semibold">
+                  {genre.name}
+                </div>
+                <div className="textColor1 text-center text-xl font-medium">
+                  {genre.genre}
+                </div>
+              </Link>
+            ))}
           </div>
         </div>
       </div>
