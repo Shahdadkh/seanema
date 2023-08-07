91b53526c74bc17e2370cd526cf5b7d20c868067
ShahdadKh <mr.khoshbakht91@gmail.com>
2023-08-07 14:48:43 +0330

    Create TvShowPage

---
+++ src/components/Home/TvShowPage/PopularChannelCarousel/EmblaCarousel.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import useEmblaCarousel, { EmblaOptionsType } from "embla-carousel-react";
+import embla from "./css/embla.module.css";
+
+import pic1 from "@/assets/images/channel/nbc.png";
+import pic2 from "@/assets/images/channel/ngn.png";
+import pic3 from "@/assets/images/channel/nickelodeon.png";
+import pic4 from "@/assets/images/channel/fox-movies.png";
+import pic5 from "@/assets/images/channel/national-geographic.png";
+import pic6 from "@/assets/images/channel/cn.png";
+import pic7 from "@/assets/images/channel/cnn.png";
+import pic8 from "@/assets/images/channel/hd.png";
+
+type PropType = {
+  slides: number[];
+  options?: EmblaOptionsType;
+};
+
+const EmblaCarousel: React.FC<PropType> = (props) => {
+  const { slides, options } = props;
+  const [emblaRef] = useEmblaCarousel(options);
+
+  const images: any = [pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8];
+
+  return (
+    <div className={embla.embla}>
+      <div className={embla["embla__viewport"]} ref={emblaRef}>
+        <div className={embla["embla__container"]}>
+          {images.map((item: any, index: number) => (
+            <div className={embla["embla__slide"]} key={index}>
+              <img
+                className={embla["embla__slide__img"]}
+                src={item.src}
+                alt={(index + 1).toString()}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default EmblaCarousel;
--- src/components/Home/TvShowPage/TvShowPage.tsx
+++ src/components/Home/TvShowPage/TvShowPage.tsx
@@ -1,7 +1,23 @@
+import TitlePage from "@/components/Common/TitlePage";
+import TitlePageWithSeeAll from "@/components/Common/TitlePageWithSeeAll";
+import PopularChannelCarousel from "./PopularChannelCarousel/PopularChannelCarousel";
+
 const TvShowPage = () => {
   return (
     <div className="w-full h-screen sm:w-3/12 mx-auto overflow-x-hidden backgroundColor1">
-      <h1 className="text-white">TvShowPage</h1>
+      <div className="mt-10">
+        <TitlePage title="Tv Show" />
+      </div>
+      <div className="mx-2 sm:mx-8 mt-14">
+        <TitlePageWithSeeAll title="Popular Channel" src="" />
+      </div>
+      <div className="mt-8 ml-14">
+        <PopularChannelCarousel />
+      </div>
+      <div className="text-white text-3xl font-medium mx-14 mt-12 mb-4">
+        Now Playing
+      </div>
+      <div>ss</div>
     </div>
   );
 };
